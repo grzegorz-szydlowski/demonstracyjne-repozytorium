b9f6a3d7830e22f2eafbb62d62853d2cc62d7fd3
grzegorz-szydlowski <grzegorz.szydlowski237@gmail.com>
2024-12-04 11:44:55 +0100

    First commit on branch1

---
+++ ConsoleWriter.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace demonstracyjne_repozytorium
+{
+public static class ConsoleWriter 
+    {
+        public static void Write(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
